fd97afe275916447511a0de47c508bd860cd9907
ridwanullahi <ridwanullahiajayi01@gmail.com.com>
2022-12-21 09:11:54 +0100

    complete

--- queries.sql
+++ queries.sql
@@ -1,3 +1,25 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals
+WHERE name LIKE '%mon';
+
+SELECT name FROM animals
+WHERE extract(year from date_of_birth) BETWEEN '2016' AND '2019';
+
+SELECT name FROM animals
+WHERE neutered = TRUE AND escape_attempts < 3;
+
+SELECT TO_CHAR(date_of_birth:: date,  'Mon dd, yyyy') AS birth FROM animals
+WHERE name = 'Agumon' or name = 'Pikachu';
+
+SELECT name from animals
+WHERE weight_kg > 10.5 ;
+
+SELECT * FROM animals
+WHERE neutered = TRUE;
+
+SELECT * FROM animals
+WHERE name != 'Gabumon';
+
+SELECT * FROM animals
+WHERE weight_kg = 10.4 OR weight_kg <= 17.3
--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE animals(
+    id INTEGER NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts integer NOT NULL,
+    neutered BOOLEAN NOT NULL,
+    weight_kg FLOAT NOT NULL	
 );
