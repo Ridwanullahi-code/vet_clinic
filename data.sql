72028082dc77688717c6fedc2e35e1aab6453415
ridwanullahi <ridwanullahiajayi01@gmail.com.com>
2022-12-22 21:51:01 +0100

    complete day2

--- data.sql
+++ data.sql
@@ -2,9 +2,24 @@ SET datestyle = dmy;
 
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES(1, 'Agumon','02/03/2020',0,true,10.23);
+
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES(2, 'Gabumon','15/11/2018',2,true,8.0);
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES(3, 'Pikachu','07/01/2021',1,false,15.04);
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES(4, 'Devimon','12/05/2017',5,true,11.0);
+INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES(5,'Charmander','08/02/2020',0,false,-11.0);
+INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES(6,'Plantmon','15/11/2021',2,true,-5.7);
+INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES(7,'Squirtle','02/04/1993',3,false,-12.13);
+INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES(8,'Angemon','12/06/2005',1,true,-45.0);
+INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES(9,'Boarmon','07/06/2005',7,true,20.4);
+INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES(10,'Blossom','13/10/1998',3,true,17.0);
+INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES(11,'Ditto','14/05/2022',4,true,22.0);
